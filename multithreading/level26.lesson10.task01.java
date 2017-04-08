542e59782923f3418bf16b68d3d2458bae9f8bd2
cold-f <cold-f@ya.ru>
2017-04-08 14:39:14 +0300

    lock, unlock, ReentrantLock

--- multithreading/level26.lesson10.task01.java
+++ multithreading/level26.lesson10.task01.java
@@ -3,7 +3,7 @@ package com.javarush.test.level26.lesson10.task01;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-/* Весь мир играет комедию.
+/* Весь мир играет комедию.//
 Почитать про java.util.concurrent.locks.Lock на http://docs.oracle.com/  (там все есть в джавадоках!)
 Написать реализацию метода someMethod:
 1. попытаться захватить лок
