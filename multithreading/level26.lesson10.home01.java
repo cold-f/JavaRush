f105aa38671548b00b1cb5759f6e1a6e8ed8a7dd
cold-f <cold-f@ya.ru>
2017-04-08 17:21:31 +0300

    BlockingQueue блокируюущая очередь,executorService

---
+++ multithreading/level26.lesson10.home01.java
@@ -0,0 +1,65 @@
+package com.javarush.test.level26.lesson10.home01;
+
+/* Мир скучен для скучных людей.
+Разберитесь с BlockingQueue
+По образу и подобию класса Producer создайте класс Consumer, который будет выводить данные из BlockingQueue в консоль
+*/
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        BlockingQueue queue = new ArrayBlockingQueue(32);
+
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(producer);
+        executorService.submit(consumer);
+
+        Thread.sleep(2000);
+
+        executorService.shutdownNow();
+
+    }
+}
+************************************************
+public class Producer implements Runnable {
+    protected BlockingQueue queue;
+
+    public Producer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            int i = 0;
+            while (true) {
+                queue.put(String.valueOf(i++));
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
+************************************************
+public class Consumer implements Runnable {
+    protected BlockingQueue queue;
+
+    public Consumer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+    @Override
+    public void run() {
+        try {
+
+            while (true) {
+                System.out.println(queue.poll());
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
