2ad199ce3dec595713cf57bb45dd2ced8b749cb2
cold-f <cold-f@ya.ru>
2017-05-05 10:41:01 +0300

    Callable, Future, ExecutorService

---
+++ multithreading/level28.lesson10.task01.java
@@ -0,0 +1,44 @@
+package com.javarush.test.level28.lesson10.task01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/* Осваиваем Callable
+Реализуйте логику метода getTask, который должен возвращать объект Callable.
+Объект Callable должен вычислять сумму всех чисел от 1 до переданного числа i включая его, и возвращать его в виде строки*/
+public class Solution {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Future<String>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 1000; i <= 1010; i++) {
+            futures.add(executor.submit(getTask(i)));
+        }
+
+        futures.add(executor.submit(getTask(10000000)));
+
+        for(Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+
+    }
+
+    public static Callable<String> getTask(final int i) {
+
+        return new Callable<String>()
+        {
+            long sum = 0;
+            @Override
+            public String call() throws Exception
+            {
+                for (int j = 1; j <= i; j++){
+                    sum += j;
+                }
+                return String.valueOf(sum);
+            }
+        };
+    }
+}
