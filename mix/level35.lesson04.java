77d84e96ab336f5c42793336b15e5679807f8dc8
cold-f <cold-f@ya.ru>
2017-07-13 18:49:51 +0300

    Generics, wildcard задачи

--- mix/level35.lesson04.java
+++ mix/level35.lesson04.java
@@ -123,3 +123,24 @@ public class Solution {
 [com.javarush.test.level35.lesson08.task01.Solution$C@61bbe9ba, com.javarush.test.level35.lesson08.task01.Solution$B@610455d6]
         [com.javarush.test.level35.lesson08.task01.Solution$C@61bbe9ba]
         */
+        
+*****************************************************************************************************************************
+*****************************************************************************************************************************
+/* extends vs super
+Логика всех методов - добавить source в destination.
+!!!Расставьте ?, extends и super где необходимо:!!!
+1) one - должен работать с одним и тем же типом;
+2) two - должен добавлять любых наследников типа T в список, умеющий хранить только тип T;
+3) three - должен добавлять объекты типа T в любой список, параметризированный любым родительским классом;
+4) four - должен добавлять любых наследников типа T в список, параметризированный любым родительским классом.
+Не оставляйте закомментированный код.
+*/
+public abstract class Solution {
+    public abstract <T> void one(List<T> destination, List<T> source);
+
+    public abstract <T> void two(List<T> destination, List<? extends T> source);
+
+    public abstract <T> void three(List<? super T> destination, List<T> source);
+
+    public abstract <T> void four(List<? super T> destination, List<? extends T> source);
+}
